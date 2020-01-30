4e72942902bc0b01616c966cc507b07fd6fe2c4a
Danila Balagansky <d.balagansky@me.com>
2020-01-31 00:12:51 +0300

    Fix maimcmd

--- config.h
+++ config.h
@@ -61,12 +61,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "uxterm", NULL };
 static const char *slockcmd[]  = { "slock", NULL };
+static const char *maimcmd[] = { "sh", "-c", "maim -s -k --format png | xclip -selection clipboard -t image/png", NULL };
 
 static Key keys[] = {
         /* modifier                     key        function        argument */
         { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
         { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
         { MODKEY,                       XK_s,      spawn,          {.v = slockcmd } },
+        { MODKEY,                       XK_c,      spawn,          {.v = maimcmd } },
         { MODKEY,                       XK_b,      togglebar,      {0} },
         { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
         { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
